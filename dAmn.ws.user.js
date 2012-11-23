d638d392cb848e590d975b156073045d13023d03
Joshua Blume <blumeusa@hotmail.com>
2012-11-23 01:03:44 -0500

    Changed the encoding method to escape/unescape

--- dAmn.ws.user.js
+++ dAmn.ws.user.js
@@ -3,7 +3,7 @@
 // @namespace      botdom.com
 // @description    Make the official client use WebSockets
 // @author         Henry Rapley <photofroggy@gmail.com>
-// @version        1.0.1
+// @version        1.0.2
 // @include        http://chat.deviantart.com/chat/*
 // @include        http://chat.deviantart.lan/chat/*
 // ==/UserScript==
@@ -50,7 +50,7 @@ var dAmnWebSocket = function(  ) {
                     });
                     
                     this.sock.onmessage = function( event ) {
-                        dAmn_DoCommand( 'data', decodeURIComponent(event.data.split('+').join(' ')) );
+                        dAmn_DoCommand( 'data', unescape(event.data.split('+').join(' ')) );
                     };
                     
                     this.sock.onclose = function( event ) {
@@ -76,7 +76,7 @@ var dAmnWebSocket = function(  ) {
                             return;
                         }
                     }
-                    this.sock.send(encodeURIComponent(cmd.arg).split(' ').join('+'));
+                    this.sock.send(escape(cmd.arg).split(' ').join('+'));
                     break;
                 case 'ping':
                     break;
